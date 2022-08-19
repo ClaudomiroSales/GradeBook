0c6f90322d4903ad5c0e81658cee0367a57feb57
ClaudomiroSales <claudomiro.sales@gmail.com>
2022-08-19 09:52:22 -0300

    Uso de statics
    Usando GradeBook:: egradebook0.

--- main.cpp
+++ main.cpp
@@ -16,25 +16,11 @@ int main()
    const GradeBook GRADEBOOK5( GRADEBOOK2 );
    const GradeBook GRADEBOOK6( gradebook3 );
 
-   cout << "\nDuas versoes do metodo displayMessage: const e nao const\n";
-   GRADEBOOK2.displayMessage( );
-   GRADEBOOK5.displayMessage( );
-   gradebook3.displayMessage( );//Objects não const podem usar métodos const
-
-   //ERROR: tentando usar método não const
-   //GRADEBOOK5.setNumAlunos( 10 );
-
-   //Variável pertence a classe
-   cout << "\nTestando atributos statics.\n";
-   cout << "Alunos em GradeBook3 " << gradebook3.getNumAlunos( ) << '\n';
-
-   //gradebook3( "Geography", 10 )
-   //GRADEBOOK6( gradebook3 )
-   //Não pode ter 10 alunos pois ultrapassa o número máximo de alunos
-   cout << "Alunos em GradeBook6 " << GRADEBOOK6.getNumAlunos( ) << '\n';
-
-   cout << "Alunos que conseguiram vaga na escola " << gradebook3.getnumAlunosEscola( ) << '\n';
-   cout << "Numero de GradeBooks instanciados: "    << gradebook3.getnumGradeBooks( ) << '\n';
+   //Static: A variável pertence a classe
+   cout << "\nTestando metodos statics.\n";
+   cout << "Alunos que conseguiram vaga na escola GradeBook:: " << GradeBook::getNumAlunosEscola( ) << '\n';
+   cout << "Alunos que conseguiram vaga na escola GRADEBOOK6. " << GRADEBOOK6.getNumAlunosEscola( ) << '\n';
+   cout << "Numero de GradeBooks instanciados: "    << GradeBook::getnumGradeBooks( ) << '\n';
    cout << "Numero de GradeBooks instanciados: "    << gradebook0.getnumGradeBooks( ) << '\n';
    
    return 0;
