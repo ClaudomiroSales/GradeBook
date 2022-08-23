7c64476205d97e3377881cb839d2bef1a0c08171
ClaudomiroSales <claudomiro.sales@gmail.com>
2022-08-23 16:10:56 -0300

    Update main.cpp
    Trabalhando ponteiros.
    Inicialização.
    Atribuição
    Operador ->
    Impressão de endereço

--- main.cpp
+++ main.cpp
@@ -11,7 +11,15 @@ int main()
    GradeBook gradebook1( "Math", 3 );
    const GradeBook GRADEBOOK2( "Physics", 20 );
    GradeBook gradebook3( "Geography", 70 );
-   GradeBook *gradebookPtr;   
+   GradeBook *gradebookPtr;
+   //Ponteiro em C++
+   //GradeBook *gradebookPtr;
+   //Pode inicializar já com um endereço válido 
+   //GradeBook *gradebookPtr = &gradebook1;
+   //Ou inserir o endereço depois
+   gradebookPtr = &gradebook1;
+
+
    //Lista de alunos
    const int NUMALUNOS = 4;
    const string alunos[ NUMALUNOS ] = { "Catarina", "Sophia", "Noemi", "Goo"};
@@ -19,10 +27,17 @@ int main()
    for( int i = 0; i < NUMALUNOS; i++ )
         cout << alunos[i] << endl;
    
+   //Cadastrando alunos para gradebook1
    gradebook1.cadastrarAlunoGradeBook( alunos[ 0 ] );
    gradebook1.cadastrarAlunoGradeBook( alunos[ 1 ] );
-   gradebook1.cadastrarAlunoGradeBook( alunos[ 2 ] );
+      //Cadastrando alunos em gradebook1 usando gradebookPtr
+   gradebookPtr->cadastrarAlunoGradeBook( alunos[ 2 ] );
    gradebook1.displayMessage( );
-   
+   cout << "Imprimindo lista de alunos de gradebook1 usando o ponteiro gradebookPtr.\n";
+   gradebookPtr->displayMessage( );
+   cout << "Endereco de gradebook1: " << gradebookPtr << endl;
+   gradebookPtr = &gradebook3;
+   cout << "Agora gradebookPtr aponta para gradebook3.\n";
+   cout << "Endereco de gradebook3: " << gradebookPtr << endl;
    return 0;
 }
